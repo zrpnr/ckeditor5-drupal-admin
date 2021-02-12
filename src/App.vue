34df70aaf05881db4f03f0ef2eb53adf1a1c4ca3
zrpnr <peter.weber@acquia.com>
2021-02-11 19:23:48 -0700

    Basics of sortable in place

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,83 @@
 <template>
-  <Toolbar :title="titleAvailable" :list="listAvailable" />
-  <Toolbar :title="titleSelected" :list="listSelected" />
+  <div class="ckeditor5-toolbar--disabled">
+    <div class="ckeditor5-toolbar-available">
+      <label for="ckeditor5-toolbar-available__buttons"
+        >Available buttons</label
+      >
+      <draggable
+        class="ckeditor5-toolbar-available__buttons"
+        :list="listAvailable"
+        :group="group"
+        itemKey="id"
+      >
+        <template #item="{ element }">
+          <ToolbarButton :id="element.id" :name="element.name" />
+        </template>
+      </draggable>
+    </div>
+    <div class="ckeditor5-toolbar-divider">
+      <label for="ckeditor5-toolbar-divider__buttons"
+        >Button divider</label
+      >
+      <draggable
+        class="ckeditor5-toolbar-divider__buttons"
+        :list="dividers"
+        :group="{ name: 'toolbar', put: false, pull: 'clone', sort: 'false' }"
+        itemKey="id"
+      >
+        <template #item="{ element }">
+          <ToolbarButton :id="element.id" :name="element.name" />
+        </template>
+      </draggable>
+    </div>
+  </div>
+  <div class="ckeditor5-toolbar-active">
+    <label for="ckeditor5-toolbar-active__buttons">Active toolbar</label>
+    <draggable
+      class="ckeditor5-toolbar-active__buttons"
+      :list="listSelected"
+      :group="group"
+      itemKey="id"
+    >
+      <template #item="{ element }">
+        <ToolbarButton :id="element.id" :name="element.name" />
+      </template>
+    </draggable>
+  </div>
 </template>
 
 <script setup>
-  import Toolbar from './components/Toolbar.vue';
-  import { computed } from 'vue';
-  
-  // init
-  const available = document.getElementById('buttons-available');
-  const selected = document.getElementById('buttons-selected');
+import { ref } from "vue";
+import draggable from "vuedraggable";
+import ToolbarButton from './components/ToolbarButton.vue';
 
-  const availableToolbar = JSON.parse(`{"toolbar": ${available.innerHTML} }`);
-  const selectedToolbar = JSON.parse(`{"toolbar": ${selected.value} }`);
-  [available, selected].forEach((el) => {
-    el.style.visibility = 'hidden';
-  });
+// init
+const available = document.getElementById("buttons-available");
+const selected = document.getElementById("buttons-selected");
+[available, selected].forEach((el) => {
+  el.style.visibility = "hidden";
+});
 
-  // basic setup
-  const titleAvailable = "Available buttons";
-  const listAvailable = computed(() => {
-    return availableToolbar.toolbar.filter((item) => !selectedToolbar.toolbar.includes(item));
-  });
+// @todo: validate the provided values.
+const availableToolbar = JSON.parse(`{"toolbar": ${available.innerHTML} }`);
+const selectedToolbar = JSON.parse(`{"toolbar": ${selected.value} }`);
 
-  const titleSelected = "Selected Toolbar buttons"
-  const listSelected = selectedToolbar.toolbar;
+
+// basic setup
+const group = "toolbar";
+const dividers = [{ name: "|", id: "divider" }, { name: "-", id: 'wrapping' }];
+const selectedButtons = [...selectedToolbar.toolbar];
+const availableButtons = availableToolbar.toolbar.filter((item) => !selectedButtons.includes(item));
+
+const listSelected = ref(selectedButtons.map((item) => ({ name: item, id: item })));
+const listAvailable = ref(availableButtons.map((item) => ({ name: item, id: item })));
 
 </script>
+
+<style scoped>
+  .ckeditor5-toolbar--disabled {
+    margin-bottom: 2rem;
+    display: flex;
+    gap: 2rem;
+  }
+</style>
---
+++ src/components/ToolbarButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <span :class="selector">{{ name }}</span>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  name: String,
+  id: String,
+});
+
+const selector = `ckeditor5-toolbar__button ckeditor5-toolbar__button-${props.id}`;
+
+</script>
+
+<style scoped>
+.ckeditor5-toolbar__button {
+  display: inline-block;
+  margin: 0 .2rem;
+  padding: .2rem .4rem;
+  background: #eee;
+  border: 1px solid #999;
+  border-radius: 4px;
+  cursor: grab;
+}
+</style>
