ea0ee5df3cc79498ca70e14cd46de0d4dd90326b
zrpnr <peter.weber@acquia.com>
2021-02-11 17:43:21 -0700

    Very basic toolbar display

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Toolbar :title="titleAvailable" :list="listAvailable" />
+  <Toolbar :title="titleSelected" :list="listSelected" />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+  import Toolbar from './components/Toolbar.vue';
+  import { computed } from 'vue';
+  
+  // init
+  const available = document.getElementById('buttons-available');
+  const selected = document.getElementById('buttons-selected');
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  const availableToolbar = JSON.parse(`{"toolbar": ${available.innerHTML} }`);
+  const selectedToolbar = JSON.parse(`{"toolbar": ${selected.value} }`);
+  [available, selected].forEach((el) => {
+    el.style.visibility = 'hidden';
+  });
+
+  // basic setup
+  const titleAvailable = "Available buttons";
+  const listAvailable = computed(() => {
+    return availableToolbar.toolbar.filter((item) => !selectedToolbar.toolbar.includes(item));
+  });
+
+  const titleSelected = "Selected Toolbar buttons"
+  const listSelected = selectedToolbar.toolbar;
+
+</script>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <h2>{{ title }}</h2>  
+  <ul>
+    <li v-for="item in list" :key="item">{{ item }}</li>
+  </ul>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  title: String,
+  list: Array
+});
+
+</script>
+
