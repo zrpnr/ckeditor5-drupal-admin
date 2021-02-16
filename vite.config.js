7ff29d4fa11304ea3585469b7f6b2f12bc935bb9
zrpnr <peter.weber@acquia.com>
2021-02-16 15:53:51 -0700

    Export a umd, externalize sortable and vue for smaller build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import vue from '@vitejs/plugin-vue'
+const path = require('path')
 
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default {
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.js'),
+      name: 'CKEditor5Admin',
+    },
+    rollupOptions: {
+      external: ['sortablejs', 'vue'],
+      output: {
+        globals: {
+          sortablejs: 'Sortable',
+          vue: 'Vue',
+        }
+      },
+    }
+  }
 }
